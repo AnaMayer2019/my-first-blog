1002739ee9ed03a6aea348d01964f3b7eeb83f00
Your Name <you@example.com>
2019-11-05 17:58:11 -0500

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('about/', views.aboutpage, name='about')
+    path('about/', views.aboutpage, name='about'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
